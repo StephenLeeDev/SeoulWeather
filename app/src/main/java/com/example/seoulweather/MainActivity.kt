a704ea4db327a6aadb09d8bb8bcc8630afb32068
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-10-27 12:20:29 +0900

    Edit | Change LocationManager to FusedLocationProviderClient

--- app/src/main/java/com/example/seoulweather/MainActivity.kt
+++ app/src/main/java/com/example/seoulweather/MainActivity.kt
@@ -2,83 +2,81 @@ package com.example.seoulweather
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
 import android.content.pm.PackageManager
-import android.location.Location
-import android.location.LocationListener
-import android.location.LocationManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.example.seoulweather.data.model.LatLng
-
-const val PERMISSION_LOCATION: Int = 1001
+import com.example.seoulweather.databinding.ActivityMainBinding
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.CancellationTokenSource
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var locationManager: LocationManager
-    private lateinit var myLocationListener: MyLocationListener
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private var cancellationTokenSource: CancellationTokenSource? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-        val isGpsEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
-        if (isGpsEnabled) {
-            if (ContextCompat.checkSelfPermission(
-                            this,
-                            Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED) {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(
-                                this,
-                                Manifest.permission.ACCESS_FINE_LOCATION
-                        )) {
-                } else {
-                    ActivityCompat.requestPermissions(
-                            this,
-                            arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
-                            PERMISSION_LOCATION
-                    )
-                }
-            } else {
-                setMyLocationListener()
-            }
-        }
-
+        initVariables()
+        requestLocationPermission()
     }
 
     @SuppressLint("MissingPermission")
-    private fun setMyLocationListener() {
-        val minTime = 1500L
-        val minDistance = 100f
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if (::myLocationListener.isInitialized.not()) {
-            myLocationListener = MyLocationListener()
-        }
-        with(locationManager) {
-            requestLocationUpdates(
-                    LocationManager.GPS_PROVIDER,
-                    minTime,
-                    minDistance,
-                    myLocationListener
-            )
-            requestLocationUpdates(
-                    LocationManager.NETWORK_PROVIDER,
-                    minTime,
-                    minDistance,
-                    myLocationListener
-            )
+        val locationPermissionGranted =
+            requestCode == REQUEST_ACCESS_LOCATION_PERMISSION &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED
+
+        if (locationPermissionGranted) {
+            cancellationTokenSource = CancellationTokenSource()
+
+            cancellationTokenSource?.let { tokenSource ->
+                fusedLocationProviderClient.getCurrentLocation(
+                    LocationRequest.PRIORITY_HIGH_ACCURACY,
+                    tokenSource.token
+                ).addOnSuccessListener { location ->
+                    binding.textView.text = "${location.latitude}, ${location.longitude}"
+                }
+            }
+        } else {
+            // TODO : 다이얼로그 팝업으로 권한 재요청 및 앱 종료 선택하도록 유도
         }
     }
 
-    inner class MyLocationListener: LocationListener {
-        override fun onLocationChanged(location: Location) {
-            val locationLatLngEntity = LatLng(
-                    location.latitude.toFloat(),
-                    location.longitude.toFloat()
-            )
-        }
+    override fun onDestroy() {
+        super.onDestroy()
+        cancellationTokenSource?.cancel()
+    }
+
+    private fun initVariables() {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+    }
+
+    private fun requestLocationPermission() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ),
+            REQUEST_ACCESS_LOCATION_PERMISSION
+        )
+    }
+
+    companion object {
+        private const val REQUEST_ACCESS_LOCATION_PERMISSION = 1000
+        private const val REQUEST_BACKGROUND_ACCESS_LOCATION_PERMISSION = 1001
     }
 }
