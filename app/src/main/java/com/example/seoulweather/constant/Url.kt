a2f6beb7f3ccf376507e971f974b3248b2bf6f26
StephenLeeDev <stephen.lee.dev@gmail.com>
2021-10-27 12:21:48 +0900

    Add | Generate urls object class

---
+++ app/src/main/java/com/example/seoulweather/constant/Url.kt
@@ -0,0 +1,7 @@
+package com.example.seoulweather.constant
+
+object Url {
+    const val KAKAO_API_BASE_URL = "https://dapi.kakao.com/"
+    const val AIR_KOREA_API_BASE_URL = "http://apis.data.go.kr/"
+    const val OPEN_WEATHER_API_BASE_URL = "https://api.openweathermap.org/"
+}
